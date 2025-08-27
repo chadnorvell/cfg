162bdf8ed39d9f06fa49a114d2bebc10be64b108
Chad Norvell <chadnorvell@pm.me>
2025-08-26 21:14:53 -0700

    karuna: map tab to mt(meh)

--- nix/native/hosts/karuna.nix
+++ nix/native/hosts/karuna.nix
@@ -58,7 +58,18 @@
     enable = true;
     settings = {
       device_name = "AT Translated Set 2 keyboard";
+      dual_role = [
+        {
+          input = "KEY_TAB";
+          tap = ["KEY_TAB"];
+          hold = ["KEY_LEFTCTRL" "KEY_LEFTALT" "KEY_LEFTSHIFT"];
+        }
+      ];
       remap = [
+        {
+          input = ["KEY_LEFTCTRL"];
+          output = ["KEY_LEFTCTRL" "KEY_LEFTSHIFT"];
+        }
         {
           input = ["KEY_CAPSLOCK"];
           output = ["KEY_LEFTCTRL"];
