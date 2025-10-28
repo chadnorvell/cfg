d15cc7c8665a04426466547da064113c5d7a6de5
Chad Norvell <chadnorvell@pm.me>
2025-10-28 10:39:15 -0700

    sunyata->khazaana

--- nix/native/hosts/sunyata.nix
+++ nix/native/hosts/sunyata.nix
@@ -43,7 +43,7 @@
     fsType = "cifs";
     options =  let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in ["${automount_opts},credentials=/etc/nixos/samba_credentials/khazaana,uid=1000,gid=100"];
+      in ["${automount_opts},credentials=/etc/nixos/samba_credentials/khazaana,uid=1000,gid=100,forcegid"];
   };
 
   swapDevices = [
