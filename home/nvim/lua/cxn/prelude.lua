04fbddb7779ecb600265264c77e7766f9b762a6b
Chad Norvell <chadnorvell@pm.me>
2025-10-28 22:24:04 -0700

    nvim: improvements

--- home/nvim/lua/cxn/prelude.lua
+++ home/nvim/lua/cxn/prelude.lua
@@ -17,6 +17,8 @@ vim.opt.splitright = true
 vim.opt.timeoutlen = 300
 vim.opt.undofile = true
 vim.opt.updatetime = 250
+vim.o.sessionoptions =
+  "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.g.have_nerd_font = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -32,28 +34,28 @@ vim.opt.softtabstop = 0
 
 -- Overrides for 2-space tab stop file formats
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = {
-        "*.css",
-        "*.ex",
-        "*.exs",
-        "*.nix",
-        "*.rb",
+  pattern = {
+    "*.css",
+    "*.ex",
+    "*.exs",
+    "*.nix",
+    "*.rb",
 
-        "*.js",
-        "*.cjs",
-        "*.mjs",
-        "*.jsx",
-        "*.ts",
-        "*.cts",
-        "*.mts",
-        "*.tsx",
-        "*.json",
-        "*.jsonc",
-    },
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 3
-    end,
+    "*.js",
+    "*.cjs",
+    "*.mjs",
+    "*.jsx",
+    "*.ts",
+    "*.cts",
+    "*.mts",
+    "*.tsx",
+    "*.json",
+    "*.jsonc",
+  },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 3
+  end,
 })
 
 vim.filetype.add({ extension = { fidl = "fidl" } })
@@ -61,10 +63,7 @@ vim.filetype.add({ extension = { fidl = "fidl" } })
 -- Share the system clipboard
 -- Loaded async because otherwise it can increase startup time
 vim.schedule(function()
-    vim.opt.clipboard = "unnamedplus"
+  vim.opt.clipboard = "unnamedplus"
 end)
 
-vim.diagnostic.config({
-    signs = true,
-    virtual_lines = { only_current_line = true },
-})
+vim.diagnostic.config({ signs = true })
--- home/nvim/lua/plugins/snacks.lua
+++ home/nvim/lua/plugins/snacks.lua
@@ -1,54 +1,54 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    dependencies = {
-        "echasnovski/mini.nvim",
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  dependencies = {
+    "echasnovski/mini.nvim",
+  },
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    picker = { enabled = true },
+    quickfile = { enabled = false },
+    scope = { enabled = true },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    -- bufdelete
+    {
+      "<leader>bd",
+      function()
+        require("snacks").bufdelete.delete()
+      end,
+      desc = "kill",
     },
-    opts = {
-        bigfile = { enabled = true },
-        dashboard = { enabled = false },
-        explorer = { enabled = false },
-        indent = { enabled = true },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        picker = { enabled = true },
-        quickfile = { enabled = false },
-        scope = { enabled = true },
-        scroll = { enabled = false },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },
+    {
+      "<leader>bD",
+      function()
+        require("snacks").bufdelete.other()
+      end,
+      desc = "kill others",
     },
-    keys = {
-        -- bufdelete
-        {
-            "<leader>bd",
-            function()
-                require("snacks").bufdelete.delete()
-            end,
-            desc = "kill",
-        },
-        {
-            "<leader>bD",
-            function()
-                require("snacks").bufdelete.other()
-            end,
-            desc = "kill others",
-        },
-        {
-            "<leader>b<BS>",
-            function()
-                require("snacks").bufdelete.all()
-            end,
-            desc = "kill all",
-        },
-        -- notifier
-        {
-            "<leader>xn",
-            function()
-                require("snacks").notifier.show_history()
-            end,
-            desc = "notifications",
-        },
+    {
+      "<leader>b<BS>",
+      function()
+        require("snacks").bufdelete.all()
+      end,
+      desc = "kill all",
     },
+    -- notifier
+    {
+      "<leader>xn",
+      function()
+        require("snacks").notifier.show_history()
+      end,
+      desc = "notifications",
+    },
+  },
 }
