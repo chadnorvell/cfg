e2dfae4127e94f1b6855cf85ef0297d64852fbee
Chad Norvell <chadnorvell@pm.me>
2025-10-28 18:39:08 -0700

    mazama update

--- nix/native/hosts/mazama.nix
+++ nix/native/hosts/mazama.nix
@@ -1,29 +1,44 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "thunderbolt"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1defb5f2-cad0-4210-b961-0458fe3c984f";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/1defb5f2-cad0-4210-b961-0458fe3c984f";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/843D-2E4E";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/a6d47ef1-c47a-4b2d-b28f-099808b69fb1"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/843D-2E4E";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/a6d47ef1-c47a-4b2d-b28f-099808b69fb1"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
@@ -42,16 +57,20 @@
   services.power-profiles-daemon.enable = true;
   services.fwupd.enable = true;
   hardware.framework.laptop13.audioEnhancement = {
-    enable = true;
+    enable = false;
     hideRawDevice = true;
     rawDeviceName = "alsa_input.pci-0000_c1_00.6.analog-stereo";
   };
 
   services.logind = {
-    powerKey = "suspend-then-hibernate";
-    lidSwitch = "suspend-then-hibernate";
-    lidSwitchExternalPower = "suspend-then-hibernate";
-    lidSwitchDocked = "ignore";
+    settings = {
+      Login = {
+        HandleLidSwitch = "suspend-then-hibernate";
+        HandleLidSwitchExternalPower = "suspend-then-hibernate";
+        HandleLidSwitchDocked = "ignore";
+        HandlePowerKey = "suspend-then-hibernate";
+      };
+    };
   };
 
   systemd.sleep = {
@@ -68,18 +87,25 @@
       dual_role = [
         {
           input = "KEY_TAB";
-          tap = ["KEY_TAB"];
-          hold = ["KEY_LEFTCTRL" "KEY_LEFTALT" "KEY_LEFTSHIFT"];
+          tap = [ "KEY_TAB" ];
+          hold = [
+            "KEY_LEFTCTRL"
+            "KEY_LEFTALT"
+            "KEY_LEFTSHIFT"
+          ];
         }
       ];
       remap = [
         {
-          input = ["KEY_LEFTCTRL"];
-          output = ["KEY_LEFTCTRL" "KEY_LEFTSHIFT"];
+          input = [ "KEY_LEFTCTRL" ];
+          output = [
+            "KEY_LEFTCTRL"
+            "KEY_LEFTSHIFT"
+          ];
         }
         {
-          input = ["KEY_CAPSLOCK"];
-          output = ["KEY_LEFTCTRL"];
+          input = [ "KEY_CAPSLOCK" ];
+          output = [ "KEY_LEFTCTRL" ];
         }
       ];
     };
